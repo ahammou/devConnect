b733db12ddd001c1198ada778f7d8aa09c1f43c3
ahammou <hammou.anouar@gmail.com>
2020-01-30 11:20:36 +0100

    profile models and routes done

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('../../middleware/auth')
 
-// @route   GET api/profile
-// @desc    Test route
-// @access  Public
-router.get('/', (req, res) => { res.send('profile route') })
+const Profile = require('../../models/Profile')
+// @route   GET api/profile/me
+// @desc    Get current users profile
+// @access  Private
+router.get('/', auth, (req, res) => { r
+    try {
+        const profile = await Profile.findOne({ user: req.user.id })
+    } catch(err) {
+        console.error(err.message)
+        res.status(500).send('Server error')
+    }
+})
 
 module.exports = router
